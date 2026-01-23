2fc66ab621f159f0c32c9712de0883465ae5b10b
Ömer KÜLAK <omer.kulak@kitayazilim.com>
2026-01-23 12:27:41 +0300

    fix info

--- payment_paytr_kt/__manifest__.py
+++ payment_paytr_kt/__manifest__.py
@@ -1,28 +1,28 @@
 # Part of Kitayazilim. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Payment Provider: PayTR iFrame',
-    'version': '1.3',
-    'category': 'Accounting/Payment Providers',
-    'sequence': 350,
-    'summary': "A PayTR payment provider.",
-    "author": "Kıta Yazılım",
+    "name": "Payment Provider: PayTR iFrame",
+    "version": "1.3",
+    "category": "Accounting/Payment Providers",
+    "sequence": 350,
+    "summary": "A PayTR payment provider.",
+    "author": "Kıta",
     "website": "https://kitayazilim.com",
-    'depends': ['payment', 'account_payment', 'sale'],
-    'data': [
-        'data/payment_provider_data.xml',
-        'views/payment_provider_views.xml',
+    "depends": ["payment", "account_payment", "sale"],
+    "data": [
+        "data/payment_provider_data.xml",
+        "views/payment_provider_views.xml",
     ],
-    'application': False,
-    'post_init_hook': 'post_init_hook',
-    'uninstall_hook': 'uninstall_hook',
-    'assets': {
-        'web.assets_frontend': [
-            'payment_paytr_kt/static/src/js/payment_form.js',
+    "application": False,
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
+    "assets": {
+        "web.assets_frontend": [
+            "payment_paytr_kt/static/src/js/payment_form.js",
         ],
     },
-    'license': 'LGPL-3',
-    'images': [
-        'static/description/PayTR_kita.png',
+    "license": "LGPL-3",
+    "images": [
+        "static/description/PayTR_kita.png",
     ],
 }
